59c6d08389197731410f6749534e28b520808268
Cedric Villanueva <javaman144@gmail.com>
2021-03-07 18:13:41 +0800

    fix typo for user login struct

--- blockchain-backend/src/models/user/User.go
+++ blockchain-backend/src/models/user/User.go
@@ -3,5 +3,5 @@ package user
 // Login model is binded on the /login function, must contain username and password in json payloads
 type Login struct {
 	Username string `json:"username"`
-	Password string `json:"password`
+	Password string `json:"password"`
 }
